583ef5ebe99c0b6cc86dca7f2f8841301f138e95
huyphung1602 <huy.phung.sw@gmail.com>
2020-02-02 00:54:20 +0700

    Hiding the map edge

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,12 +10,20 @@ public class CameraController : MonoBehaviour
     private Vector3 bottomLeftLimit;
     private Vector3 topRightLimit;
 
+    private float halfHeight;
+    private float halfWidth;
+
     // Start is called before the first frame update
     void Start()
     {
         target = PlayerController.instance.transform;
-        bottomLeftLimit = theMap.localBounds.min;
-        topRightLimit = theMap.localBounds.max;
+
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+
+        theMap.CompressBounds();
+        bottomLeftLimit = theMap.localBounds.min + new Vector3(halfWidth, halfHeight, 0f);
+        topRightLimit = theMap.localBounds.max - new Vector3(halfWidth, halfHeight, 0f);
     }
 
     // LateUpdate is called once per frame after Update
